0b7e199c2ba6c16c28a39eb921eaae6b7ca3cba3
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-11 23:27:27 +0900

    update profile service adding update user profile api

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,14 +1,41 @@
 import { supabase } from "@/lib/supabase";
+import type { Database, Tables } from "@/types/supabase.types";
 
-export async function getUserProfile(userId: string) {
+type ProfilesRow = Tables<"user_profiles">;
+type ProfilesUpdate = Database["public"]["Tables"]["user_profiles"]["Update"];
+export type UserProfile = Pick<
+  ProfilesRow,
+  | "id"
+  | "avatar_url"
+  | "user_name"
+  | "interests"
+  | "languages"
+  | "updated_at"
+  | "created_at"
+>;
+
+export async function getUserProfile(userId: string): Promise<UserProfile> {
   const profileBuilder = supabase.from("user_profiles").select("*").single();
 
   const { data, error } = await profileBuilder;
 
   if (error) throw error;
-  return data;
+  return data as UserProfile;
 }
 
-export async function updateProfile(userId: string) {
-  // const updateProfileBuilder = supabase.from("profile").update({ avatar_url : })
+export async function updateUserProfile(
+  payload: ProfilesUpdate
+): Promise<UserProfile> {
+  const updatePayload = { ...payload, updated_at: new Date().toISOString() };
+  const updateProfileBuilder = supabase
+    .from("user_profiles")
+    .update(updatePayload)
+    .select("id, avatar_url, user_name, interests, languages")
+    .single();
+
+  const { data, error } = await updateProfileBuilder;
+
+  if (error) throw error;
+
+  return data as UserProfile;
 }
