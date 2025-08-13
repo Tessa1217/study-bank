b4a6df90066ada787bee36680621721b23db7237
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-13 23:18:23 +0900

    update app.jsx adding global alert component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useFetchProfile } from "@/hooks/useFetchProfile.ts";
 import { RouterProvider } from "react-router-dom";
+import GlobalAlert from "@/components/feedback/global-alert";
 import router from "@/routes/routes";
 function App() {
   useFetchProfile();
   return (
     <>
       <RouterProvider router={router}></RouterProvider>
+      <GlobalAlert />
     </>
   );
 }
