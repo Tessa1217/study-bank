ec2ac9c6619cb53c0b33da335fe390d4d4b04b70
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-15 05:12:57 +0900

    add auth query

---
+++ src/hooks/queries/useAuthQuery.ts
@@ -0,0 +1,55 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { signInOAuth, signInWithPassword, signOut, signUp } from "@/api/auth.api";
+import type { Provider } from "@/api/auth.api";
+
+export type Credential = {
+  email: string;
+  password: string;
+};
+
+export const useSignUpMutation = ({email, password}:{email: string; password: string}) => {
+  const navigate = useNavigate()
+  return useMutation({
+    mutationFn: async () => {
+      const { data, error } = await signUp({email, password})
+      return data
+    },
+    onSuccess: () => {
+      navigate("/auth/login") 
+    }
+  })
+}
+
+export const useSignInWithPasswordMutation = ({email, password}:{email: string; password: string;}) => {
+  const navigate = useNavigate()
+  return useMutation({
+    mutationFn: async () => {
+      const { data } = await signInWithPassword({email, password})
+      return data
+    },
+    onSuccess: () => {
+      navigate("/") 
+    }
+  })
+}
+
+export const useSignInWithOAuthMutation = (provider:Provider) => {
+  return useMutation({
+    mutationFn: async () => {
+      await signInOAuth(provider)
+    },
+  })
+}
+
+export const useSignOut = () => {
+  const navigate = useNavigate()
+  return useMutation({
+    mutationFn: async () => {
+      await signOut()
+    },
+    onSuccess: () => {
+      navigate("/auth/login")
+    }
+  })
+}
