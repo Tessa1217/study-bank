b91153cbec0fb19a4c27485c06f92c307c8207d0
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-15 05:16:26 +0900

    add auth api layer

---
+++ src/api/auth.api.ts
@@ -0,0 +1,29 @@
+import { supabase } from "@/lib/supabase";
+
+export type Credentials = {
+  email: string;
+  password: string;
+}
+
+export type Provider = "github" | "google"
+
+export async function signInWithPassword(credentials:Credentials) {
+  return await supabase.auth.signInWithPassword(credentials)  
+}
+
+export async function signInOAuth(provider:Provider) {
+  return await supabase.auth.signInWithOAuth({provider, options: {
+    redirectTo : "/"
+  }})
+}
+
+export async function signUp(credentials:Credentials) {
+  return await supabase.auth.signUp({
+    ...credentials,
+    options: { emailRedirectTo : `${import.meta.env.VITE_APP_URL}` }    
+  })
+}
+
+export async function signOut() {
+  return await supabase.auth.signOut()
+}
