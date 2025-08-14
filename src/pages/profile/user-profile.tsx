863add9ab917dac37cf4d0f93142d50076ca9d1d
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-15 05:16:43 +0900

    update user-profile page -- still in progress

--- src/pages/profile/user-profile.tsx
+++ src/pages/profile/user-profile.tsx
@@ -1,5 +1,17 @@
+import { useAuthStore } from "@/store/useAuthStore";
+import { Navigate } from "react-router-dom";
 const UserProfile = () => {
-  return <div>Profile</div>;
+  const profile = useAuthStore((state) => state.profile);
+
+  if (!profile) {
+    return <Navigate to="/auth/login" />;
+  }
+
+  return (
+    <div className="space-y-6">
+      <h2 className="text-2xl font-bold">사용자 설정</h2>
+    </div>
+  );
 };
 
 export default UserProfile;
