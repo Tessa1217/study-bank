408deaa258743fb5cc6cacf7e88d7912157847cb
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-14 20:17:26 +0900

    add query provider for react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "@/styles/style.css";
+const qc = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={qc}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
