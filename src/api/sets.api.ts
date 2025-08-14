ca697051d74bc5102b8accfd614bdd0b15ae53a7
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-14 20:18:24 +0900

    update set api

--- src/api/sets.api.ts
+++ src/api/sets.api.ts
@@ -1,16 +1,31 @@
+import type { StudyCardRow } from "@/api/cards.api";
 import type { SbResult } from "@/api/shared/sb-api";
 import { sbExecMany, sbExecOne, supabase } from "@/api/shared/sb-api";
-import type { Row, Insert } from "@/api/shared/types";
+import type { Row } from "@/api/shared/types";
 export type StudySetRow = Row<"study_set">
-type StudySetUpsert = Insert<"study_set">
-
-
-export async function getStudySet(setId:string):SbResult<StudySetRow> {
-  const setBuilder = supabase.from("study_set").select("*").eq("id", setId).single()
-  return sbExecOne(setBuilder)
+export interface StudySetWithCard extends Partial<StudySetRow> {
+  study_card : Partial<StudyCardRow>[]
 }
 
-export async function upsertStudySet(payload:StudySetUpsert):SbResult<StudySetRow> {
-  const setBuilder = supabase.from("study_set").upsert(payload).select().single()
+export async function getStudySetList():SbResult<Partial<StudySetRow>[]> {
+  const setBuilder = supabase.from("study_set").select("id, title, description")
+  return sbExecMany(setBuilder)
+}
+
+export async function getStudySet(setId:string):SbResult<StudySetWithCard> {
+  const setBuilder = supabase.from("study_set")
+                             .select(`
+                                id,
+                                title,
+                                description,
+                                study_card (
+                                  id,
+                                  word,                                  
+                                  definition,
+                                  sort_order,
+                                  word_lang,
+                                  definition_lang,
+                                  set_id
+                                )`).eq("id", setId).single()
   return sbExecOne(setBuilder)
 }
