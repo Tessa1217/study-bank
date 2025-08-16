55cc7e0eedf7ea79344d20b365a0184c010aad63
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-17 00:13:02 +0900

    update auth store

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,12 +1,11 @@
-import type { Tables } from "@/types/supabase.types";
 import type { User } from "@supabase/supabase-js";
+import type { UserProfile } from "@/api/mapper/types";
 import { createSubscribedZustandStore } from "@/store/store";
-type Profile = Tables<"user_profiles">;
 
 type AuthState = {
   isLoggedIn: boolean;
   user: User | null;
-  profile: Profile | null;
+  profile: UserProfile | null;
 };
 
 type AuthAction = {
@@ -14,7 +13,7 @@ type AuthAction = {
   logout: () => void;
   updateIsLoggedIn: (isLoggedIn: boolean) => void;
   setUser: (user: User | null) => void;
-  setProfile: (profile: Profile | null) => void;
+  setProfile: (profile: UserProfile | null) => void;
 };
 
 const initialState = {
