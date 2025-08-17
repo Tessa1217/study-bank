5337f7888b6e72459c197493040138424e9635e9
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-18 01:31:27 +0900

    update file input

--- src/components/input/file-input.tsx
+++ src/components/input/file-input.tsx
@@ -1,5 +1,41 @@
-import { Input } from "@/components/input/input";
+import {
+  getInputClasses,
+  type BasicInputProps,
+} from "@/components/input/input-styles";
+import { forwardRef, type ChangeEvent } from "react";
 
-const FileInput = () => {};
+interface FileInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    BasicInputProps {
+  onSelected?: (files: FileList) => void;
+}
+
+const FileInput = forwardRef<HTMLInputElement, FileInputProps>(
+  ({ inputWidth, inputSize, variant, disabled, onSelected, ...props }, ref) => {
+    const onFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files && e.target.files.length > 0) {
+        onSelected?.(e.target.files);
+        e.target.value = "";
+      }
+    };
+
+    const inputClass = getInputClasses({
+      inputWidth,
+      inputSize,
+      variant,
+      disabled,
+    });
+
+    return (
+      <input
+        type="file"
+        ref={ref}
+        onChange={onFileChange}
+        className={inputClass}
+        {...props}
+      />
+    );
+  }
+);
 
 export default FileInput;
