1deabffab07b31cdfa569905944a80e01973dfd8
yj.kwon <yj.kwon@fusionsoft.co.kr>
2025-08-21 15:58:03 +0900

    update app rendering with app wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App.jsx";
+import AppWrapper from "./AppWrapper.jsx";
 import "@/styles/style.css";
-const qc = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={qc}>
-      <App />
-    </QueryClientProvider>
+    <AppWrapper />
   </StrictMode>
 );
