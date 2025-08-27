398d7bceed9427d423e7e334bd2a1d39f9efd0c9
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-28 01:54:30 +0900

    update folder adding features of adding and removing study sets from folder

--- src/pages/folder/folder-add-new-set.tsx
+++ src/pages/folder/folder-add-new-set.tsx
@@ -1,24 +1,26 @@
 import type { RefObject } from "react";
-import { useSetListPublicOrCreatedByUserQuery } from "@/hooks/queries/useSetQuery";
 import { useAuthStore } from "@/store/useAuthStore";
-import Button from "@/components/button/button";
-import { SetList } from "@/components/set/set-list";
-import Dialog from "@/components/ui/dialog";
+import { useFolderAvailableSetsQuery } from "@/hooks/queries/useFolderSetQuery";
 import { X } from "lucide-react";
+import Button from "@/components/button/button";
+import SetList from "@/components/set/set-list";
+import Dialog from "@/components/ui/dialog";
 
 type FolderAddNewSetProps = {
+  folderId: string;
   open: boolean;
   setOpen: (open: boolean) => void;
   buttonRef: RefObject<HTMLButtonElement | null>;
 };
 
 export default function FolderAddNewSet({
+  folderId,
   open,
   setOpen,
   buttonRef,
 }: FolderAddNewSetProps) {
   const userId = useAuthStore((state) => state.profile?.id);
-  const { data: setList } = useSetListPublicOrCreatedByUserQuery(userId);
+  const { data: setList } = useFolderAvailableSetsQuery(userId, folderId);
 
   return (
     <Dialog
@@ -32,13 +34,12 @@ export default function FolderAddNewSet({
         <div
           ref={panelRef}
           className="
-              w-full max-w-5xl h-full max-h-[min(70vh,700px)] bg-white shadow-soft ring-1 ring-slate-200
-              focus:outline-none sm:rounded-2xl rounded-t-2xl text-left
-              
+             w-full max-w-5xl h-full max-h-[min(70vh,700px)] bg-white shadow-soft ring-1 ring-slate-200
+             focus:outline-none sm:rounded-2xl rounded-t-2xl text-left
           "
           tabIndex={0}
         >
-          <div className="px-5 pt-5 sm:px-6 sm:pt-6">
+          <div className="px-5 pt-5 sm:px-6 sm:pt-6 h-full">
             <div className="flex items-start justify-between gap-4">
               <h2 id="m-title" className="text-base sm:text-lg font-semibold">
                 학습 자료 추가하기
@@ -53,11 +54,25 @@ export default function FolderAddNewSet({
                 <X />
               </Button>
             </div>
-            <div
-              id="m-desc"
-              className="px-5 pb-4 pt-3 sm:px-6 sm:pb-4 sm:pt-4 text-slate-700 overflow-hidden"
-            >
-              <SetList setList={setList} />
+            <div className="overflow-y-auto h-[80%]">
+              <div
+                id="m-desc"
+                className="px-5 pb-4 pt-3 sm:px-6 sm:pb-4 sm:pt-4 text-slate-700"
+              >
+                <div className="h-full">
+                  <SetList folderId={folderId} setList={setList} />
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-end p-4">
+              <Button
+                color="primary"
+                size="md"
+                className="rounded-full"
+                onClick={close}
+              >
+                추가 완료
+              </Button>
             </div>
           </div>
         </div>
--- src/pages/folder/folder.tsx
+++ src/pages/folder/folder.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useRef } from "react";
+import { lazy, Suspense, useMemo, useRef, type RefObject } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useFolderQuery } from "@/hooks/queries/useFolderQuery";
 import { useToggle } from "@/hooks/useToggle";
@@ -7,6 +7,8 @@ import Button from "@/components/button/button";
 import PageWrapper from "@/components/layout/page-wrapper";
 import PageHeader from "@/components/layout/page-header";
 import PageButtonContainer from "@/components/layout/page-button-container";
+import { useFolderSetsQuery } from "@/hooks/queries/useFolderSetQuery";
+import SetList from "@/components/set/set-list";
 
 const FolderAddNewSetModal = lazy(
   () => import("@/pages/folder/folder-add-new-set")
@@ -16,8 +18,13 @@ export default function FolderPage() {
   const { folderId } = useParams();
   const navigate = useNavigate();
   const { data: folder, isLoading } = useFolderQuery(folderId);
+  const { data: setList } = useFolderSetsQuery(folderId);
   const [open, setOpen] = useToggle(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
+  const setAddedToFolder = useMemo(
+    () => setList?.filter(({ addedToFolder }) => !!addedToFolder) || [],
+    [setList]
+  );
 
   // 렌더링 분기
   if (isLoading) {
@@ -58,36 +65,74 @@ export default function FolderPage() {
           <Button color="primary" onClick={() => navigate("/set/new")}>
             학습 세트 만들기
           </Button>
-        </PageButtonContainer>
-      </PageHeader>
-      <div className="flex p-10 flex-col gap-3 justify-center items-center bg-white m-1">
-        <div className="flex">
-          <BookA size={30} />
-          <BookA size={30} />
-          <BookA size={30} />
-        </div>
-        <p>학습 자료를 추가해서 폴더를 완성해보세요.</p>
-        <Button
-          ref={buttonRef}
-          color="primary"
-          variant="outline"
-          className="flex gap-3"
-          onClick={() => setOpen(true)}
-        >
-          학습 자료 추가하기
-          <Plus />
-        </Button>
-        {open && (
-          <Suspense fallback={<div>Loading...</div>}>
-            <FolderAddNewSetModal
+          {setAddedToFolder.length > 0 && (
+            <AddStudySetButton
               open={open}
               setOpen={setOpen}
               buttonRef={buttonRef}
+              folderId={folder.id!}
             />
-          </Suspense>
+          )}
+        </PageButtonContainer>
+      </PageHeader>
+      <div className="flex p-10 flex-col gap-3 justify-center items-center bg-white m-1">
+        {setAddedToFolder.length > 0 ? (
+          <SetList folderId={folder.id!} setList={setAddedToFolder} />
+        ) : (
+          <>
+            <div className="flex">
+              <BookA size={30} />
+              <BookA size={30} />
+              <BookA size={30} />
+            </div>
+            <p>학습 자료를 추가해서 폴더를 완성해보세요.</p>
+            <AddStudySetButton
+              open={open}
+              setOpen={setOpen}
+              buttonRef={buttonRef}
+              folderId={folder.id!}
+            />
+          </>
         )}
       </div>
     </PageWrapper>
+  );
+}
+
+function AddStudySetButton({
+  folderId,
+  buttonRef,
+  open,
+  setOpen,
+}: {
+  folderId: string;
+  buttonRef: RefObject<HTMLButtonElement | null>;
+  open: boolean;
+  setOpen: (v?: boolean) => void;
+}) {
+  return (
+    <>
+      <Button
+        ref={buttonRef}
+        color="primary"
+        variant="outline"
+        className="flex gap-3"
+        onClick={() => setOpen(true)}
+      >
+        학습 자료 추가하기
+        <Plus />
+      </Button>
+      {open && (
+        <Suspense fallback={""}>
+          <FolderAddNewSetModal
+            folderId={folderId}
+            open={open}
+            setOpen={setOpen}
+            buttonRef={buttonRef}
+          />
+        </Suspense>
+      )}
+    </>
   );
 }
 
