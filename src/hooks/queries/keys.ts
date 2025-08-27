d5b5aebb4ac108903465f9f6811af55bc916f8aa
Tessa1217 <kwonyuj1996@gmail.com>
2025-08-27 23:25:50 +0900

    update query keys and set query for adding set relations query

--- src/hooks/queries/keys.ts
+++ src/hooks/queries/keys.ts
@@ -1,26 +1,28 @@
 export const setsKeys = {
-  all: ['sets'] as const,
-  list: () => [...setsKeys.all, 'list'] as const,
-  detail: (id: string) => [...setsKeys.all, 'detail', id] as const,
+  all: ["sets"] as const,
+  list: () => [...setsKeys.all, "list"] as const,
+  listPublicOrCreatedByUser: (userId: string) =>
+    [...setsKeys.all, "listPublicOrCreatedByUser", userId] as const,
+  detail: (id: string) => [...setsKeys.all, "detail", id] as const,
 };
 
 export const foldersKeys = {
-  all: ['folders'] as const,
-  list: () => [...foldersKeys.all, 'list'] as const,
-  detail: (id: string) => [...foldersKeys.all, 'detail', id] as const,
+  all: ["folders"] as const,
+  list: () => [...foldersKeys.all, "list"] as const,
+  detail: (id: string) => [...foldersKeys.all, "detail", id] as const,
 };
 
 export const setWithCardsKeys = {
-  all: ['setWithCards'] as const,
-  detail: (id: string) => [...setWithCardsKeys.all, 'detail', id] as const,
+  all: ["setWithCards"] as const,
+  detail: (id: string) => [...setWithCardsKeys.all, "detail", id] as const,
 };
 
 export const cardKeys = {
-  all: ['cards'] as const,
-  list: (setId:string) => [...cardKeys.all, 'list', setId] as const,  
-}
+  all: ["cards"] as const,
+  list: (setId: string) => [...cardKeys.all, "list", setId] as const,
+};
 
 export const profileKeys = {
-  all: ['user'] as const,
-  detail: (userId:string) => [...profileKeys.all, 'detail', userId] as const
-}
+  all: ["user"] as const,
+  detail: (userId: string) => [...profileKeys.all, "detail", userId] as const,
+};
--- src/hooks/queries/useSetQuery.ts
+++ src/hooks/queries/useSetQuery.ts
@@ -1,7 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
-import { setsKeys } from '@/hooks/queries/keys';
-import { getStudySets } from '@/api/sets.api';
-import { toSetSummary } from '@/api/mapper/mapper';
+import { useQuery } from "@tanstack/react-query";
+import { setsKeys } from "@/hooks/queries/keys";
+import {
+  getStudySets,
+  getStudySetsPublicOrCreatedByUser,
+} from "@/api/sets.api";
+import { toSetItem, toSetSummary } from "@/api/mapper/mapper";
 
 /** 리스트 쿼리 */
 export function useSetListQuery() {
@@ -16,3 +19,20 @@ export function useSetListQuery() {
   });
 }
 
+export function useSetListPublicOrCreatedByUserQuery(
+  userId: string | undefined
+) {
+  return useQuery({
+    queryKey: userId
+      ? setsKeys.listPublicOrCreatedByUser(userId)
+      : setsKeys.listPublicOrCreatedByUser(""),
+    enabled: !!userId,
+    queryFn: async () => {
+      if (!userId) throw new Error("Missing user id");
+      const res = await getStudySetsPublicOrCreatedByUser(userId);
+      if (res.error) throw res.error;
+      return res.data!;
+    },
+    select: (rows) => rows?.map(toSetItem),
+  });
+}
