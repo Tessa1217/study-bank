1b2c47c19ece00b639d2dc905cd9cb8818ba5db0
Tessa1217 <kwonyuj1996@gmail.com>
2025-09-19 02:49:36 +0900

    fix : game match session api file name typeo fixed

--- src/api/study-match-game-session.api.ts
+++ src/api/study-match-game-session.api.ts
@@ -1,28 +1,13 @@
-import { supabase } from "@/api/shared/sb-api";
-import { sbExecOne } from "@/api/shared/sb-api";
-import type { Insert, Row } from "@/api/shared/types";
-
-export type StudyMatchGameSessionRow = Row<"study_match_game_session">;
-export type StudyMatchGameSessionInsert = Insert<"study_match_game_session">;
+import type { StudyMatchGameSessionInsert } from "@/api/repository/studyMatchGameSession.repository";
+import { studyMatchGameSessionRepository } from "@/api/repository/studyMatchGameSession.repository";
 
 export async function insertStudyMatchGameSession(
   payload: StudyMatchGameSessionInsert
 ) {
-  const studyMatchGameSessionBuilder = supabase
-    .from("study_match_game_session")
-    .upsert(payload)
-    .select()
-    .single();
-  return sbExecOne(studyMatchGameSessionBuilder);
+  const { data } = await studyMatchGameSessionRepository.create(payload);
+  return data?.id!;
 }
 
 export async function getStudyMatchGameSession(setId: string, userId: string) {
-  const studyMatchGameSessionBuilder = supabase
-    .from("study_match_game_session")
-    .select("*")
-    .eq("set_id", setId)
-    .eq("user_id", userId)
-    .select()
-    .single();
-  return studyMatchGameSessionBuilder;
+  return studyMatchGameSessionRepository.findBySetIdAndUserId(setId, userId);
 }
